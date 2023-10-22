05219753bde6d8b59aa626099fc13eb27424bedc
hessam_khoobkar <amirhessam.dev@gmail.com>
2023-10-23 02:59:13 +0330

    update atts from html to jsx

--- assets/AddItem.tsx
+++ assets/AddItem.tsx
@@ -10,8 +10,8 @@ export default function AddItemIcon() {
       <path
         d="M20 16H16M16 16H12M16 16V12M16 16V20M9.33334 4.45064C11.3594 3.27851 13.6593 2.66304 16 2.66664C23.364 2.66664 29.3333 8.63598 29.3333 16C29.3333 23.364 23.364 29.3333 16 29.3333C8.63601 29.3333 2.66667 23.364 2.66667 16C2.66667 13.572 3.31601 11.2933 4.45067 9.33331"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
+        strokeWidth="2"
+        strokeLinecap="round"
       />
     </svg>
   );
