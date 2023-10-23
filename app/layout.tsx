07ea000dbe2e091c982e22c5b375fad01946f4c9
hessam_khoobkar <amirhessam.dev@gmail.com>
2023-10-23 16:48:53 +0330

    move background color to body level

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`bg-neutral-100 text-black ${inter.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
